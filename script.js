e2620e73df170d14ec616fe0c627e9218b009874
Suraj Kumar Manna <126694317+Suraj00345@users.noreply.github.com>
2023-11-26 18:36:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
 //your JS code here. If required.
+const browserInfo = "You are using " + navigator.appName + " version " + navigator.appVersion;
+document.getElementById("browser-info").innerHTML = browserInfo;
